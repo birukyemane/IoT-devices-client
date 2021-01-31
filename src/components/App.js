e0f335fc405ef6da46c0e1e060d92d99cd6e78f4
birukyemane <bir_yem@yahoo.com>
2021-01-31 19:43:00 +0200

    - add handleAdd

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Button from '@material-ui/core/Button';
 
 import Header from './ui/Header';
 import Table from './ui/Table';
@@ -14,16 +13,37 @@ function App() {
       url: 'https://demo-iot-device-management.azurewebsites.net/api/devices',
     })
     .then(function (response) {
-      console.log(response.data)
+      console.log('fetched',response.data)
       setData(response.data)
     })
     .catch(err => console.log(err))
   }, []);
 
+  const handleAdd =  (name, modelId, serialNumber) => {
+    axios.post('https://demo-iot-device-management.azurewebsites.net/api/devices/', {
+      name,
+      modelId,
+      serialNumber
+    })
+    .then((response) => {
+      if(response.data !== {}){
+
+        const newData = data.concat({
+          name,
+          modelId,
+          serialNumber
+        });
+     
+        setData(newData);
+      }
+    })
+    .catch(err => console.log(err));
+  }
+
   return (
     <div>
         <Header />
-        <Table data={data} setData={setData}/>
+        <Table data={data} setData={setData} onAdd={handleAdd}/>
     </div>
   );
 }
